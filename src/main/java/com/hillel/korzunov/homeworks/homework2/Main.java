be2725907ec6654a501a69aea5ced13b6e9a7aa1
Korzunov Nikolay <korzunov.nikolay@gmail.com>
2022-09-06 16:15:01 +0300

    added source code

---
+++ src/main/java/com/hillel/korzunov/homeworks/homework2/Main.java
@@ -0,0 +1,19 @@
+package com.hillel.korzunov.homeworks.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte degrees=15;
+        char degreeSign=176;
+        short minutes = 40;
+        char oneQuote=39;
+        float seconds = 10.661f;
+        char latitude= 78;
+        int longitudeDegrees=87;
+        byte longitudeMinutes = 8;
+        double longitudeSeconds = 32.244;
+        char quotes=34;
+        String longitude="W";
+        System.out.println(""+degrees+degreeSign+" "+minutes+oneQuote+" "+seconds+quotes+" "+latitude+" "+longitudeDegrees+degreeSign+" "+longitudeMinutes+oneQuote+" "+longitudeSeconds+quotes+" "+longitude);
+    }
+}
