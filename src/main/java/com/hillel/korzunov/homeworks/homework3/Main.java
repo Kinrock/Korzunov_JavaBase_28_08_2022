dcceae658876738f59b64728406fc47b33e1cf00
Korzunov Nikolay <korzunov.nikolay@gmail.com>
2022-09-06 19:05:48 +0300

    Homework 3

--- src/main/java/com/hillel/korzunov/homeworks/homework3/Main.java
+++ src/main/java/com/hillel/korzunov/homeworks/homework3/Main.java
@@ -2,6 +2,13 @@ package com.hillel.korzunov.homeworks.homework3;
 
 public class Main {
     public static void main(String[] args) {
+    int edge1=12;
+    int edge2=24;
+    int edge3=6;
+    int volume=edge1*edge2*edge3;
+    int length=4*(edge1+edge2+edge3);
+    System.out.println("ќб'Їм паралелеп≥педа = " + volume);
+    System.out.println("—умарна довжина вс≥х стор≥н  = " + length);
 
     }
 }
